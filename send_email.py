b925b6c3ae5146ab1b0932f2ddfeb03131f52865
Pawan Manjhi <manjhipawankumar26@gmail.com>
2024-01-15 00:36:36 +0530

    Add send email feature

---
+++ send_email.py
@@ -0,0 +1,21 @@
+import smtplib, ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "manjhipawankumar26@gmail.com"
+password = "wlwjknmijcigfksy"
+
+receiver = "manjhipawankumar26@gmail.com"
+my_context = ssl.create_default_context()
+
+message = """\
+Subject: test
+Hi!
+How are you?
+Bye!
+"""
+
+with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
